88610786c94acfae6915181296189016690839e7
yukihirokinjo <yukihirokinjo@gmail.com>
2020-12-12 21:46:13 +0900

    Updata file

--- TCRF/TranscriptomeRarefaction.bash
+++ TCRF/TranscriptomeRarefaction.bash
@@ -1,5 +1,5 @@
 #
-# TranscriptomeRarefaction.sh 
+# TranscriptomeRarefaction.bash 
 #
 # depends on:
 #   Trinity
@@ -19,9 +19,9 @@ version=1.0
 
 usage_exit() {
   echo "#" 1>&2
-  echo "# TranscriptomeRarefaction.sh  version-${version}  " 1>&2
+  echo "# TranscriptomeRarefaction.bash  version-${version}  " 1>&2
   echo "#" 1>&2
-  echo "# Usage: TranscriptomeRarefaction.sh  -i Trinity.fasta_file -m gene_trans_map_file -1 read_R1.fq -2 read_R2.fq -o output_directory  " 1>&2
+  echo "# Usage: TranscriptomeRarefaction.bash  -i Trinity.fasta_file -m gene_trans_map_file -1 read_R1.fq -2 read_R2.fq -o output_directory  " 1>&2
   echo "#" 1>&2
   echo "# Options: " 1>&2
   echo "# -i Transcript sequence file (Trinity.fasta) " 1>&2
@@ -35,8 +35,8 @@ usage_exit() {
   echo "# Requirements: " 1>&2
   echo "# - Trinity"    1>&2
   echo "# - bowtie2" 1>&2
-  echo "# - seqtk"    1>&2
   echo "# - RSEM"   1>&2
+  echo "# - Samtools"   1>&2
   exit 1
 }
 
@@ -155,67 +155,58 @@ if [ -z "$readR2" ]; then
   echo "Read file is not specified" 1>&2
   usage_exit
 fi
-if [ -z "$outDir" ]; then
-  echo "output directory is not specified" 1>&2
-  usage_exit
-fi
+
+# default output directory
+[ -z $out_dir ] && out_dir="TCRF_OUT_`date +%Y%m%d`_`date +%H%M`"
 
 
 ## prep output directory and file
 mkdir -p ./${outDir}
+Error_Check Create_OutDir
 echo "Prop. NumGenes" > ${outDir}/GeneCount.txt
 
-##  RSEM count 
-
-
-  mkdir ${outDir}/RSEM/tmp
-  mv Trinity.fasta.{RSEM,bowtie2}.* ${outDir}/RSEM/tmp
-
-
-for i in 0.001 0.01 0.05 0.1 0.2 0.3 0.5 0.75 ; do
-
-  p=`echo "scale=0; ${i} * 1000" | bc`
-  Int=${p%.*}
-  Prop=$(printf %04d $Int);
-
-  # count number of expressed genes (above the TPM threshold)
-  NumGene=$( awk -v tpm=${mtTPM:=5} '{if($6 > tpm) print $0}' ${outDir}/${Prop}/RSEM.genes.results | wc -l ) 
-  echo "${i} ${NumGene}" >>  ${outDir}/GeneCount.txt
-
-done
-
-
-
-###########################################################################
-
-## iteration 
-for i in 0.001 0.01 0.05 0.1 0.2 0.3 0.5 0.75 ; do
-
-  p=`echo "scale=0; ${i} * 1000" | bc`
-  Int=${p%.*}
-  Prop=$(printf %04d $Int);
-
-## subsample the input reads
-  seqtk sample -s ${Seed:=101} ${ReadR1} ${i} > ./${outDir}/sub_${Prop}_R1.fq
-  seqtk sample -s ${Seed:=101} ${ReadR2} ${i} > ./${outDir}/sub_${Prop}_R2.fq
-
-##  TPM count 
-  $TRINITY_HOME/util/align_and_estimate_abundance.pl \
+##  RSEM  1st (for All data)
+$TRINITY_HOME/util/align_and_estimate_abundance.pl \
               --transcripts ${InFasta}  \
               --seqType fq \
-              --left ${outDir}/sub_${Prop}_R1.fq  --right ${outDir}/sub_${Prop}_R2.fq \
+              --left ${readR1}  --right ${readR2}  \
               --est_method RSEM   \
               --aln_method bowtie2 \
               --prep_reference \
               --gene_trans_map  ${InMap} \
-              --output_dir  ${outDir}/${Prop} 
+              --thread_count ${numCPU} \
+              --output_dir  ${outDir}/RSEM_All
 
-  mkdir ${outDir}/${Prop}/tmp
-  mv Trinity.fasta.{RSEM,bowtie2}.* ${outDir}/${Prop}/tmp
-  
+Error_Check  align_and_estimate_abundance.pl
+
+mkdir ${outDir}/RSEM_All/tmp
+mv Trinity.fasta.{RSEM,bowtie2}.* ${outDir}/RSEM_All/tmp
+
+NumGene=$( awk -v tpm=${mtTPM:=5} '{if($6 > tpm) print $0}' ${outDir}/${pID}/RSEM.genes.results | wc -l ) 
+echo "100 ${NumGene}" >>  ${outDir}/GeneCount.txt
+Error_Check GeneCount
+
+##  RSEM  2nd (for subsampling)
+for i in 0.01 0.05 0.1 0.2 0.3 0.5 0.75 ; do
+  # set iteration variables
+  p=`echo "scale=0; ${i} * 100" | bc`  # 0.01 -> 1.00
+  Int=${p%.*}                           # 1.00 -> 1 (%)
+  Frac=$(printf %02d $Int);             # 1 -> 01 (%)
+ 
+  # subsample bam file
+  samtools view -s ${Seed:=101}.${Frac} -b ${outDir}/RSEM_All/bowtie2.bam.for_rsem.bam > ${outDir}/p_${Frac}.sam  # -s "SeedNum" + "." + "Proportion"
+  Error_Check SAMtools
+
+  # RSEM for each subset
+  rsem-calculate-expression --num-threads ${numCPU} --sam ${outDir}/p_${Frac}.sam --paired-end ${outDir}/RSEM_All/tmp/Trinity.fasta.RSEM  ${outDir}/p_${Frac}
+  Error_Check  RSEM
+
   # count number of expressed genes (above the TPM threshold)
-  NumGene=$( awk -v tpm=${mtTPM:=5} '{if($6 > tpm) print $0}' ${outDir}/${Prop}/RSEM.genes.results | wc -l ) 
-  echo "${i} ${NumGene}" >>  ${outDir}/GeneCount.txt
+  NumGene=$( awk -v tpm=${mtTPM:=5} '{if($6 > tpm) print $0}' ${outDir}/${Frac}/RSEM.genes.results | wc -l ) 
+  Error_Check GeneCount
+
+  # record results in output file
+  echo "${Frac} ${NumGene}" >>  ${outDir}/GeneCount.txt
 
 done
 
