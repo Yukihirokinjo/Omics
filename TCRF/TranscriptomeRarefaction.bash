dac395a036a5679c1eaee70ee03144093666b121
yukihirokinjo <yukihirokinjo@gmail.com>
2021-01-26 16:54:31 +0900

    File update

--- TCRF/TranscriptomeRarefaction.bash
+++ TCRF/TranscriptomeRarefaction.bash
@@ -17,7 +17,7 @@ Seed=""
 TPMmt=""
 numCPU=""
 Info_SamBam=""
-version=1.1
+version=2.0
 
 usage_exit() {
 cat << EOS
@@ -26,19 +26,24 @@ cat << EOS
   #
   # Usage: TranscriptomeRarefaction.bash  -i Trinity.fasta_file -m gene_trans_map_file -1 read_R1.fq -2 read_R2.fq -o output_directory  
   #
-  # Options: 
+  # Arguments (mandatory): 
   # -i Transcript sequence file (Trinity.fasta) 
   # -m gene_trans_map file (Trinity.fasta.gene_trans_map)
   # -1 read_R1   
   # -2 read_R2   
+  # Arguments (Optional): 
+  # --RSEM		use RSEM for abundance estimation (default)
+  # --Salmon	use Salmon for abundance estimation
   # -c num_CPUs 
   # -s random seed number (default: 101)
-  # -t TPM threshold (default: 2)
+  # -t read count threshold (default: 2)
   #
   # Requirements:
   # - Trinity 
   # - bowtie2
   # - RSEM
+  # - Salmon
+  # - Seqtk
   # - Samtools 
 EOS
   exit 1
@@ -121,6 +126,14 @@ do
         fi
       fi
       ;;
+    '-RSEM')
+		EstMethod="RSEM"
+		shift 1
+	  ;;
+     '-Salmon')
+		EstMethod="Salmon"
+		shift 1
+	  ;;
     '-c')
       if [ -z "$2" ]; then
         echo "PROGRAM: option requires an argument $1" 1>&2
@@ -167,62 +180,115 @@ fi
 # default output directory
 [ -z $outDir ] && outDir="TCRF_OUT_`date +%Y%m%d`_`date +%H%M`"
 
+if [ -z "$EstMethod" ]; then
+  EstMethod="Salmon"
+fi
+
 
 ## prep output directory and file
 mkdir -p ${outDir}
 Error_Check Create_OutDir
 echo "Prop. NumGenes" > ${outDir}/GeneCount.txt
 
-##  RSEM  1st (for All data)
-$TRINITY_HOME/util/align_and_estimate_abundance.pl \
-              --transcripts ${InFasta}  \
-              --seqType fq \
-              --left ${readR1}  --right ${readR2}  \
-              --est_method RSEM   \
-              --aln_method bowtie2 \
-              --prep_reference \
-              --gene_trans_map  ${InMap} \
-              --thread_count ${numCPU} \
-              --output_dir  ${outDir}/RSEM_All
+############################################################ if EstMethod == RSEM 
 
-Error_Check  align_and_estimate_abundance
+if [ ${EstMethod} == "RSEM" ] ; then
+	##  RSEM  1st (for All data)
+	$TRINITY_HOME/util/align_and_estimate_abundance.pl \
+	              --transcripts ${InFasta}  \
+	              --seqType fq \
+	              --left ${readR1}  --right ${readR2}  \
+	              --est_method RSEM   \
+	              --aln_method bowtie2 \
+	              --prep_reference \
+	              --gene_trans_map  ${InMap} \
+	              --thread_count ${numCPU:=1} \
+	              --output_dir  ${outDir}/RSEM_All
 
-mkdir ${outDir}/RSEM_All/tmp
-mv Trinity.fasta.{RSEM,bowtie2}.* ${outDir}/RSEM_All/tmp
+	Error_Check  align_and_estimate_abundance
+
+	mkdir ${outDir}/RSEM_All/tmp
+	mv Trinity.fasta.{RSEM,bowtie2}.* ${outDir}/RSEM_All/tmp
 
 
-##  RSEM  2nd (for subsampling)
-for i in 0.01 0.05 0.1 0.2 0.3 0.5 0.75 ; do
-  # set iteration variables
-  p=`echo "scale=0; ${i} * 100" | bc`   # 0.01 -> 1.00
-  Int=${p%.*}                           # 1.00 -> 1 (%)
-  Frac=$(printf %02d $Int);             # 1 -> 01 (%)
- 
-  # subsample bam file ( -s "SeedNum" + "." + "Proportion" )
-  samtools view -s ${Seed:=101}.${Frac} -b ${outDir}/RSEM_All/bowtie2.bam.for_rsem.bam > ${outDir}/p_${Frac}.sam  
-  Error_Check  SAMtools
 
-  # RSEM for each subset
-  rsem-calculate-expression --num-threads ${numCPU} --sam ${outDir}/p_${Frac}.sam --paired-end ${outDir}/RSEM_All/tmp/Trinity.fasta.RSEM  ${outDir}/p_${Frac}
-  Error_Check  RSEM
+	##  RSEM  2nd (for subsampling)
+	for i in 0.01 0.05 0.1 0.2 0.3 0.5 0.75 ; do
+	  # set iteration variables
+	  p=`echo "scale=0; ${i} * 100" | bc`   # 0.01 -> 1.00
+	  Int=${p%.*}                           # 1.00 -> 1 (%)
+	  Frac=$(printf %02d $Int);             # 1 -> 01 (%)
+	 
+	  # subsample bam file ( -s "SeedNum" + "." + "Proportion" )
+	  samtools view -s ${Seed:=101}.${Frac} -b ${outDir}/RSEM_All/bowtie2.bam.for_rsem.bam > ${outDir}/p_${Frac}.sam  
+	  Error_Check  SAMtools
 
-  # count number of expressed genes (above the TPM threshold)
-  NumGene=$( awk -v gtC=${gtCount:=2} '{if($5 > gtC) print $0}' ${outDir}/p_${Frac}.RSEM.genes.results | wc -l ) 
-  Error_Check GeneCount
+	  # RSEM for each subset
+	  rsem-calculate-expression --num-threads ${numCPU:=1} --sam ${outDir}/p_${Frac}.sam --paired-end ${outDir}/RSEM_All/tmp/Trinity.fasta.RSEM  ${outDir}/p_${Frac}
+	  Error_Check  RSEM
 
-  # record results in output file
-  echo "${Frac} ${NumGene}" >>  ${outDir}/GeneCount.txt
+	  # count number of expressed genes (above the TPM threshold)
+	  NumGene=$( awk -v gtC=${gtCount:=2} '{if($5 > gtC) print $0}' ${outDir}/p_${Frac}.RSEM.genes.results | wc -l ) 
+	  Error_Check GeneCount
 
-  if [ -z "$Info_SamBam" ]; then
-    rm  ${outDir}/p_${Frac}.{sam,bam}    
-  fi
+	  # record results in output file
+	  echo "${Frac} ${NumGene}" >>  ${outDir}/GeneCount.txt
 
-done
+	  if [ -z "$Info_SamBam" ]; then
+	    rm  ${outDir}/p_${Frac}.{sam,bam}    
+	  fi
 
-# record original gene count (all)
-NumGene=$( awk -v gtC=${gtCount:=2} '{if($6 > gtC) print $0}' ${outDir}/RSEM_All/RSEM.genes.results | wc -l ) 
-Error_Check GeneCount
-echo "100 ${NumGene}" >>  ${outDir}/GeneCount.txt
+	done
+
+	# record original gene count (all)
+	NumGene=$( awk -v gtC=${gtCount:=2} '{if($6 > gtC) print $0}' ${outDir}/RSEM_All/RSEM.genes.results | wc -l ) 
+	Error_Check GeneCount
+	echo "100 ${NumGene}" >>  ${outDir}/GeneCount.txt
+
+############################################################ if EstMethod == Salmon
+else
+	##  Slmon  1st (for All data)
+	salmon index -p ${numCPU:=1} -t ${InFasta} -i ref_idx
+	Error_Check  SalmonInDex
+
+	salmon quant -i ref_idx -l A -1 ${readR1} -2 ${readR2}  -p ${numCPU:=1} -o ${outDir}/Salmon_All --validateMappings
+	Error_Check  SalmonQuant
+
+	mkdir ${outDir}/Salmon_All/tmp
+
+	##  Salmon  2nd (for subsampling)
+	for i in 0.01 0.05 0.1 0.2 0.3 0.5 0.75 ; do
+	  # set iteration variables
+	  p=`echo "scale=0; ${i} * 100" | bc`   # 0.01 -> 1.00
+	  Int=${p%.*}                           # 1.00 -> 1 (%)
+	  Frac=$(printf %02d $Int);             # 1 -> 01 (%)
+	
+	  # subsample bam file ( -s "SeedNum" + "." + "Proportion" )
+	  seqtk sample -s100 ${readR1}  ${i}  > ${outDir}/p_${i}_R1.fastq 
+	  seqtk sample -s100 ${readR2}  ${i}  > ${outDir}/p_${i}_R2.fastq 
+	  Error_Check  Seqtk
+
+	  # salmon for each subset
+	  salmon quant -i ref_idx -l A -1 ${outDir}/p_${i}_R1.fastq -2 ${outDir}/p_${i}_R2.fastq -p ${numCPU:=1} -o ${outDir}/p_${Frac} --validateMappings
+	  Error_Check  SalmonQuant
+
+	  # count number of expressed genes (above the TPM threshold)
+	  NumGene=$( awk -v gtC=${gtCount:=2} '{if($5 > gtC) print $0}' ${outDir}/p_${Frac}/quant.sf | wc -l ) 
+	  Error_Check GeneCount
+
+	  # record results in output file
+	  echo "${Frac} ${NumGene}" >>  ${outDir}/GeneCount.txt
+
+	  rm ${outDir}/p_${i}_R{1,2}.fastq
+
+	done
+
+	NumGene=$( awk -v gtC=${gtCount:=2} '{if($5 > gtC) print $0}' ${outDir}/Salmon_All/quant.sf | wc -l ) 
+	Error_Check GeneCount
+	echo "100 ${NumGene}" >>  ${outDir}/GeneCount.txt
+
+
+fi
 
 
 
